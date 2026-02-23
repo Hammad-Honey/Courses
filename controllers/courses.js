6556f53db8cea1e35c7fe098d4c4aa8c25c503de
hammad <hammadpk@proton.me>
2026-02-23 11:12:38 +0500

    Complete Courses Controlles Saperat

--- controllers/courses.js
+++ controllers/courses.js
@@ -21,8 +21,56 @@ const getCoursebyid = (req, res) => {
 
 }
 
+const postaCourse = (req, res) => {
+    if (!req.body.name || req.body.name.length < 3) {
+        res.status(400).send(`Name is Required and should not be less than 3 characters`)
+    }
+    else {
+        console.log(req.body);
+        const course = {
+            id: courses.length + 1,
+            name: req.body.name
+
+        }
+        courses.push(course);
+        res.status(201).send(course);
+    }
+}
+
+const updateCourse = (req, res) => {
+    const course = courses.find(course => (course.id === parseInt(req.params.id)));  // Saving the Course object to modify
+    console.log(course)
+    if (course && typeof (req.body.name) === 'string' && req.body.name.length > 3)   // check if the course id exist
+    {
+        course.name = req.body.name;
+        console.log("Course Updated + " + req.body.name)
+        res.status(200).json(courses)
+    }
+    else {
+        res.status(400).send("Data Put failed");
+        console.log("Data Put failed");
+    }
+
+}
+
+const deleteCourse = (req,res)=>{
+    const course = courses.find(course => (course.id === parseInt(req.params.id))); // For Coures objec ref. in the memory
+    if(course)
+    {// To Delte We need to get index of the course in the array
+        const indexofcourse=courses.findIndex(coursIndex=> coursIndex.id===parseInt(req.params.id) );
+        console.log(indexofcourse)
+        console.log("Index of the Course being Delted");
+        courses.splice(indexofcourse,1);
+        res.status(200).send(courses);
+    }
+    else{
+        console.log('Did not find the course you mentioned');
+        res.status(400).send("Unale to delete not existed items");
+    }
+
+}
 
 
-module.exports ={
-    getAllCourses, getCoursebyid
+module.exports = {
+    getAllCourses, getCoursebyid, postaCourse, updateCourse, deleteCourse,
 };
--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,5 @@
 const express= require('express');
-const router=express.Router;
+const router=express.Router();
 const controller=require('../controllers/courses.js');
 
 
@@ -7,57 +7,9 @@ const controller=require('../controllers/courses.js');
 
 router.get('/', controller.getAllCourses);
 router.get('/:id', controller.getCoursebyid);
-
-// ============= Adding  a New Cousrse ==============
-router.post('/', (req, res) => {
-    if (!req.body.name || req.body.name.length < 3) {
-        res.status(400).send(`Name is Required and should not be less than 3 characters`)
-    }
-    else {
-        console.log(req.body);
-        const course = {
-            id: courses.length + 1,
-            name: req.body.name
-
-        }
-        courses.push(course);
-        res.status(201).send(course);
-    }
-});
-
-// =========== Modifing a course ====================
-router.put('/:id', (req, res) => {
-    const course = courses.find(course => (course.id === parseInt(req.params.id)));  // Saving the Course object to modify
-    console.log(course)
-    if (course && typeof (req.body.name) === 'string' && req.body.name.length > 3)   // check if the course id exist
-    {
-        course.name = req.body.name;
-        console.log("Course Updated + " + req.body.name)
-        res.status(200).json(courses)
-    }
-    else {
-        res.status(400).send("Data Put failed");
-        console.log("Data Put failed");
-    }
-
-});
-// ============= Deleting a course ================
-router.delete('/:id',(req,res)=>{
-    const course = courses.find(course => (course.id === parseInt(req.params.id))); // For Coures objec ref. in the memory
-    if(course)
-    {// To Delte We need to get index of the course in the array
-        const indexofcourse=courses.findIndex(coursIndex=> coursIndex.id===parseInt(req.params.id) );
-        console.log(indexofcourse)
-        console.log("Index of the Course being Delted");
-        courses.splice(indexofcourse,1);
-        res.status(200).send(courses);
-    }
-    else{
-        console.log('Did not find the course you mentioned');
-        res.status(400).send("Unale to delete not existed items");
-    }
-
-});
+router.post('/', controller.postaCourse);
+router.put('/:id', controller.updateCourse);
+router.delete('/:id',controller.deleteCourse);
 
 
 
